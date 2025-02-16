261154d025d9ff7180399840860e7c16c56df2c8
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2025-02-16 12:13:19 -0300

    Aula 2 dia 16/02/2025

--- aula1/index.js
+++ aula1/index.js
@@ -4,3 +4,41 @@ let lastname = 'Maria'
 
 console.log(firstName)
 console.log(lastname)
+
+let price = 30
+console.log(price)
+
+let itemName = 'Pen'// String
+let itemPrice = 3 // numero
+let itemAvailable = true // boolean
+let itemColor // valor indefinido - undefined
+let item2 = null // valor null
+console.log(itemName)
+
+// objetos
+let pen = {
+    itemName: 'Pen',
+    itemPrice: 3,
+    itemAvailable: true,
+    itemColor: 'Red'
+}
+pen.itemColor = 'blue'
+console.log(pen)
+console.log(pen.itemName)
+
+let friends = ['Marcos', 'Silvia', 'Elisa']
+friends[1] = 'Junior'
+console.log(friends)
+
+// funcao
+function salesStatus(Status,total) {
+  console.log('Transação ' + Status + ' Total amount $ ' + total)
+}
+salesStatus('Aprovada',50)
+
+// funcao com calculo
+
+function percentage10(price){
+    return price - (price * 10 / 100)
+}
+console.log(percentage10(40))
---
+++ aula2/index.js
@@ -0,0 +1,11 @@
+let num1 = 3
+let num2 = 3
+
+let total = num1 + num2
+console.log(total)
+
+let numero1 = '1'
+let numero2 = 1
+
+console.log(num1 === num2) // strict tipo e valor tem que ser iguais
+console.log(num1 == num2) // lose somente o valor igual
