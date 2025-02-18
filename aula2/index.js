16ef0fa355752386204c30ee900fa5bb5e2ab927
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2025-02-18 06:34:25 -0300

    else e if

--- aula2/index.js
+++ aula2/index.js
@@ -9,3 +9,26 @@ let numero2 = 1
 
 console.log(num1 === num2) // strict tipo e valor tem que ser iguais
 console.log(num1 == num2) // lose somente o valor igual
+
+// Operador ternario
+let driver = 90
+let speed = driver > 110 ? 'ABOVE' : 'BElOW'
+console.log(speed)
+
+// and e &&
+// or ou ||
+// not !
+
+let temIdadeMinima = true
+let temTituloEleitor = true
+let podevotar = temIdadeMinima && temTituloEleitor
+console.log(podevotar)
+
+let podeviajar = !podevotar
+console.log(podeviajar)
+
+// corCliente white
+let corCliente = undefined
+let corEstoque = 'Black'
+let corVendida = corCliente || corEstoque
+console.log(corVendida)
---
+++ aula2continuação/index.js
@@ -0,0 +1,9 @@
+let driverSpeed = 80
+if (driverSpeed > 110){
+    console.log('fast')
+}else if (driverSpeed > 40 && driverSpeed <= 110){
+    console.log('Ok')
+} else {
+    console.log('Slow')
+}
+    
