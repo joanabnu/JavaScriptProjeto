71500f3d47ec89aa7ba9c9d18232620d95e0c6e8
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2025-02-18 06:59:33 -0300

    switch,while,for

---
+++ aula2continuação/for.js
@@ -0,0 +1,7 @@
+for (i=0; i<=10; i++){
+    console.log('Numero ', i)
+}
+
+for(i=10; i<=20;i++){
+    console.log('Numero : ', i)
+}
---
+++ aula2continuação/switch.js
@@ -0,0 +1,15 @@
+let airport = 'MCO'
+
+switch(airport){
+    case 'MCO':
+        console.log('Orlando')
+        break
+    case 'JFK':
+        console.log('John F. Kennedy')
+        break
+    case 'SEA':
+        console.log('Seattle')
+        break
+    default:
+        console.log('Não existe')
+}
