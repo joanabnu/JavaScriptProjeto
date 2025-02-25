6cf51d0f3e42cdecd7ac53c5d61dbfcde8840836
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2025-02-25 06:56:10 -0300

    built in JavaScript

---
+++ oop/built.js
@@ -0,0 +1,9 @@
+console.log(Math.max(1,3,15,2,7,4))
+console.log(Math.min(1,2,15,2,7,4))
+
+let message = 'ich bin Joana'
+console.log(message.startsWith('ich'))
+console.log(message.length)
+message.includes('Joana')
+console.log(message.endsWith('ana'))
+
