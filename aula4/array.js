715fff7bc956f414cf6ecd083948008227dbd669
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2025-03-13 06:59:18 -0300

    array

--- aula4/array.js
+++ aula4/array.js
@@ -1,5 +1,11 @@
-const num = [7,8,9]
-num.push(10,11,12)
-num.unshift(1,2,3)
-num.splice(3,0,4,5)
+const num = [7, 8, 9]
+num.push(10, 11, 12)
+num.unshift(1, 2, 3)
+num.splice(3, 0, 4, 5)
 console.log(num)
+
+let numero = [5, 8, 8]
+numero.length = 0
+
+numero.splice(0, numero.length)
+console.log(numero)
---
+++ aula4/index.js
@@ -0,0 +1,7 @@
+let numbers = [5, 7, 8, 9, 6]
+let letters = ["a", "e", "u", "o"]
+
+all = numbers.concat(letters)
+half = all.slice(3, 5)
+console.log(all)
+console.log(half)
