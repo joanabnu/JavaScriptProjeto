d1bbbbed4b045e8218acae68557347b5ce69d33e
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2025-03-18 06:45:54 -0300

    temlondon

--- aula4/index.js
+++ aula4/index.js
@@ -5,3 +5,19 @@ all = numbers.concat(letters)
 half = all.slice(3, 5)
 console.log(all)
 console.log(half)
+
+ let clients = ['joana', 'jose','bruno']
+ console.log(clients)
+
+ let clientsJoin = (clients.join(','))
+ console.log(clientsJoin)
+
+//  clients.sort()
+clients.reverse
+ console.log(clients)
+
+ const TempLondon = [18,13,18,2]
+ const tempPositive = TempLondon.every(function(value){
+    return value >= 0 
+ })
+ console.log(tempPositive)
